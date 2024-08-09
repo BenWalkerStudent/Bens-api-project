413d7f2065d6ace049e9a09d76ff50087e0fb31a
BenWalkerStudent <bwalker33@s.tooeletech.edu>
2024-08-09 17:58:34 -0600

    first commit

---
+++ controllers/index.js
@@ -0,0 +1,17 @@
+const awesomeFunction = (Req, res) => {
+  res.send("Hello World!");
+};
+
+const tooeleTech = (Req, res) => {
+  res.send("Tooele Tech is Awesome!");
+};
+
+const bensBranch = (req,res) => {
+
+  res.send('hi im ben')
+
+
+
+}
+
+module.exports = { awesomeFunction, tooeleTech, bensBranch };
